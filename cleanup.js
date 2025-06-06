ba9c0b8ab5155ebae3af5377ae912620ce91af17
Matthieu Diehr <matthieu.diehr@resilience.care>
2025-06-06 15:57:00 +0200

    Test dist on main

--- cleanup.js
+++ cleanup.js
@@ -1,10 +1,13 @@
 
 import * as core from "@actions/core";
-import cleanup from "./lib/setup-cloudflare-warp";
+import { cleanup } from "./lib/setup-cloudflare-warp";
 
 (async () => {
   try {
     await cleanup();
+    // Explicit process.exit() to not wait hanging promises,
+    // see https://github.com/ruby/setup-ruby/issues/543
+    process.exit()
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import * as core from "@actions/core";
-import run from "./lib/setup-cloudflare-warp";
+import { run } from "./lib/setup-cloudflare-warp";
 
 (async () => {
   try {
